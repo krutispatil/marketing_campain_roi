88faa5e767d43cc01ff203ced3e69b168e479232
krutispatil <56932788+krutispatil@users.noreply.github.com>
2025-09-03 23:18:33 +0530

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -12,5 +12,5 @@ for insight in generate_insights(df):
 
 # Embed Tableau
 st.subheader("📈 Interactive Tableau Dashboard")
-tableau_url = "https://public.tableau.com/views/YourDashboardName/Dashboard1?:embed=y&:display_count=yes"
+tableau_url = "https://public.tableau.com/app/profile/kruti.s.patil/viz/Superstore_17265530774290/Dashboard1:embed=y&:display_count=yes"
 st.components.v1.iframe(tableau_url, width=1200, height=800)
